1d2e50d1796c536d648cd32ee4513c9106aa5d00
KRISTO RAFAEL LOPEZ ROJAS <kristo.lopez@infobox.com.pe>
2023-05-11 21:21:35 -0500

    changes_rc.sh third attepmt

--- changes_rc.sh
+++ changes_rc.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ -f rc1080p.xml ]; then
-   mv rc.xml  rc4k.xml
-   mv rc1080p.xml rc.xml
+   mv ~/.config/openbox/rc.xml ~/.config/openbox/rc.xml
+   mv ~/.config/openbox/rc.xml ~/.config/openbox/rc.xml
 else 
-   mv rc.xml rc1080p.xml
-   mv rc4k.xml rc.xml
+   mv ~/.config/openbox/rc.xml ~/.config/openbox/rc.xml
+   mv ~/.config/openbox/rc.xml ~/.config/openbox/rc.xml
 fi
