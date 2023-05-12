7880fe9b3aec7cf54f28234fdb7f64a49b901f3d
KRISTO RAFAEL LOPEZ ROJAS <kristo.lopez@infobox.com.pe>
2023-05-11 21:08:55 -0500

    changes

--- caseform.sh
+++ caseform.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-
 caseform_preboot
 notify-send "Caseform" "preboot to run project has been launched"
 caseform_kube_access &
-notify-send "Caseform" "stage database connection has established"
+notify-send "Caseform" "stage database connection has been established"
